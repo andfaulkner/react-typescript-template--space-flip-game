ec93b925955f87cf91d19c0095c0afdbab835f95
andfaulkner <andfaulkner@gmail.com>
2016-09-06 00:57:11 -0400

    fixed logging middleware, displaying diff colours for diff dispatches; reducers cleaned up

--- app/store/middleware/logger.tsx
+++ app/store/middleware/logger.tsx
@@ -1,61 +1,120 @@
 /// <reference path="../../../typings/index.d.ts" />
 
+import * as _ from "lodash";
 import moment from 'moment';
 
-enum LogTitlePosition {
+enum StartOrEnd {
   Start,
   End
-}
-
-let logTime = (msg, type?: string) => {
-  let hour = moment().get('hour');
-  let minute = moment().get('minute');
-  let second = moment().get('second');
-  let ms = moment().get('millisecond');
-
-  let ts = `T: ${hour}:${minute}:${second}.${ms}`;
-
-  let len = (70 - msg.length - ts.length);
-
-  let whitespace = (((type) ? (len - type.length) : (len)) > 1)
-      ? _.repeat(' ', ((type) ? (len - type.length) : (len)))
-      : '';
-
-  // console.log(`|| ${ts}:: ${msg}${(type) ? (' ' + type + ' ') : ''}${whitespace} ||`);
 };
 
-let blockWrapLog = (next, titlePosition: LogTitlePosition, msg: string, type?: string) => {
-  if (titlePosition === LogTitlePosition.Start) console.log(`${_.repeat('=', 80)}`);
+/**
+ * Styles to randomly select from when logging dispatches. One style is used for all logs
+ * regarding one dispatch; another is randomly selected for the following dispatch, etc..
+ * @type {Array}
+ */
+const styles = [
+  'background: #222; color: #bada55;',
+  'background: black; color: white;',
+  'font-size: 12px; color: #000; font-weight: bolder; background: yellow',
+  'font-size: 12px; color: #000; font-weight: bolder; background: lightgreen',
+  'font-size: 12px; color: #000; font-weight: bolder; background: lightblue',
+  'font-size: 12px; color: #000; font-weight: bolder; background: orange',
+  'background: #faf2eb; color: green; font-weight: 900; text-decoration: underline; border: 1px solid #111111;',
+  'background: darkred; color: white; font-weight: 900; font-size:12px' +
+      /*cont*/ '; text-shadow: -4px -4px 8px #CCC, -0.5px -0.5px 0.5px yellow',
+  'background: #000; color: yellow; font-weight: 900; font-size:12px' +
+      /*cont*/ '; text-shadow: -4px -4px 8px #CCC, -0.5px -0.5px 0.5px yellow',
+  'font-size: 12px; color: black; font-weight: 900; background: #ffccd5; border: black inset 2px' +
+      /*cont*/ '; padding: 2px; box-sizing: content-box;',
+  'color: white; background: indigo; font-size:12px; font-weight: 900' +
+      /*cont*/ '; text-shadow: 0.5px 0.5px 0.5px darkred; padding: 2px;',
+  'background: linear-gradient(#D33106, #571402); border: 1px solid #3E0E02; color: white;' +
+      /*cont*/ 'display: block; text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)' +
+      /*cont*/ '; box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5)' +
+                           ', 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset' +
+      /*cont*/ '; line-height: 40px; text-align: center; font-weight: bold; ',
+  // Below is hilarious: //
+  'color: white; background: white; font-size: 12px; font-weight: bolder' +
+     /*cont*/ '; text-shadow: 1px 0.5px 2px black, 1px -0.5px 2px black; padding: 2px;',
+];
+
+const blockWidth = 100;
+const ignoredActions = ['RESET_LAST_RENDERED_TIME', 'SET_UI_UPDATE_READY', 'CLEAR_INPUT_QUEUE'];
+// const logDoubleLine = (): void => { console.log(`${_.repeat('=', 80)}`); };
+
+const halfPad = (bookend: string, length: number, padChar: string = "="): string =>
+  `${_.repeat(padChar, length)}${bookend}`;
+
+/**
+ * Surround 'header' text with extra-emphasizing padding
+ * @param  {string} msg - header title
+ * @return {string} header string with the emphasizing padding applied
+ */
+const headerPad = (msg: string): string => {
+  const emphasizer = '||^^^';
+  const padLen = blockWidth - msg.length - (emphasizer.length * 2);
+  const lPadLen = Math.floor(padLen / 2);
+  const rPadLen = Math.ceil(padLen / 2);
+  const lPad = halfPad(emphasizer, lPadLen, '=');
+  const rPad = _.reverse(halfPad(emphasizer, rPadLen, '=').split('')).join('');
+  return `%c ${lPad} ${msg}${rPad}`;
+};
+
+/**
+ * Display the current time in a human-friendly format, optionally stylized
+ * @param  {String} msg    additional message to display beside the time
+ * @param  {String} type?: name of the action called
+ * @param  {String} style: style to apply to the message to differentiate it from other dispatches
+ */
+let logTime = (msg: string, type?: string, style: string = ''): void => {
+  const hour = moment().get('hour');
+  const minute = moment().get('minute');
+  const second = moment().get('second');
+  const ms = moment().get('millisecond');
+  const ts = `T: ${hour}:${minute}:${second}.${ms}`;
+
+  console.log(headerPad(`${ts}:: ${msg}${(type) ? (' ' + type + ' ') : ''}`), style);
+};
+
+let blockWrapLog = (next: (...args: any[]) => any, prePost: StartOrEnd, msg: string, type: string = '') => {
   if (next) {
     next(msg, (type) ? '--> ' + type : null);
-  } else {
-    if (titlePosition === LogTitlePosition.Start) console.log(msg + `: ${type}`);
+  } else if (prePost === StartOrEnd.Start) {
+    console.log(msg + `: ${type}`);
   }
-  if (titlePosition === LogTitlePosition.End) console.log(`${_.repeat('=', 80)}`);
 };
 
+/**
+ * The logging middleware. Displays info on every dispatch to the store.
+ */
 export const logger = ({ getState }) => {
-
   console.log('-------------------------------------------');
   console.log('|---  REDUX LOGGER MIDDLEWARE INJECTED  ---|');
   console.log('-------------------------------------------');
 
   return (next) => (action) => {
-    // if (!_.includes(['RESET_LAST_RENDERED_TIME', 'SET_UI_UPDATE_READY', 'CLEAR_INPUT_QUEUE'], action.type)) {
-    //   blockWrapLog(logTime, LogTitlePosition.Start, 'START action dispatch', action.type);
-    //   console.log(action.type, ': data: ', action.payload);
-    // }
+
+    // select style for this runthru of the middleware (each run gets a style randomly selected
+    // from a predefined group, to visually differentiation different calls to the store )
+    const instanceStyle = _.sample(styles);
+    const showAction = (when) => `%c || ${action.type} : ${when} dispatch: `;
+
+    // don't display anything if this was triggered by an ignored action
+    if (!_.includes(ignoredActions, action.type)) {
+      blockWrapLog(_.partialRight(logTime, instanceStyle), StartOrEnd.Start,
+                   'START action dispatch', action.type);
+      console.log(showAction('before'), instanceStyle, '; data: ', action.payload);
+    }
 
     // Call the next dispatch method in the middleware chain. Its result will then be logged.
     let returnValue = next(action);
 
-    // if (!_.includes(['RESET_LAST_RENDERED_TIME', 'SET_UI_UPDATE_READY', 'CLEAR_INPUT_QUEUE'], action.type)) {
-    //   console.log('state after dispatch: ', getState(), ';; returnValue: ', returnValue);
+    if (!_.includes(ignoredActions, action.type)) {
+      console.log(showAction('after'), instanceStyle, getState(), 'returnValue --> ', returnValue);
+    }
 
-    //   blockWrapLog(logTime, LogTitlePosition.End, 'END action dispatch', action.type);
-    // }
-
-    // Likely the action itself unless a middleware further in the chain changed it
+    // Likely the action itself is returned, unless a middleware further in the chain changed it
     return returnValue;
   };
 };
--- app/store/reducers.tsx
+++ app/store/reducers.tsx
@@ -7,26 +7,28 @@ import { resolveUIState } from './reducers/resolveUIState.tsx';
 
 import { combineReducers } from 'redux';
 
-console.log('reducers.tsx:: actions: ', actions);
-
 // All actions take this form:
 // { type: string;     payload: T;     error?: boolean;     meta?: any; }
 
-const uiStateReducers = (state: AppStoreState = initialState, action: Action<any>) => {
+const debugReducers = (state: AppStoreState = initialState, action: Action<any>) => {
   switch (action.type) {
-
     // EXPERIMENTAL REDUCER
     case actions.TEST_SWITCH_STATE:
+      console.log('*R* reducers: debugReducers # TEST_SWITCH_STATE :: Click reached reducers :)');
       return Object.assign({}, state, { testStateProperty: action.payload.testStateProperty });
+    default:
+      return state;
+  }
+};
 
-    // case actions.ADD_ITEM_TO_INPUT_QUEUE:
-    //   let newInputQueue = state.inputQueue;
-    //   newInputQueue.push(action.payload);
-    //   return Object.assign({}, state, { inputQueue: newInputQueue });
-
-    // // action: {type: "CLEAR_INPUT_QUEUE"}
-    // case actions.CLEAR_INPUT_QUEUE:
-    //   return Object.assign({}, state, { inputQueue: [] });
+/**
+ * [description]
+ * @param  {[type]} state:  AppStoreState [description]
+ * @param  {[type]} action: Action<any>   [description]
+ * @return {[type]}         [description]
+ */
+const uiStateReducers = (state: AppStoreState = initialState, action: Action<any>) => {
+  switch (action.type) {
 
     // Saves a timestamp marking when the main game arena was last rendered
     case actions.RESET_LAST_RENDERED_TIME:
@@ -47,12 +49,17 @@ const uiStateReducers = (state: AppStoreState = initialState, action: Action<any
   }
 };
 
-const inputReducers = (state: AppStoreState = initialState, action: Action<any>) => {
+type QueuePushAction = { type: string, payload?: { input: string, type: number} };
+
+const inputReducers = (state: AppStoreState = initialState, action: QueuePushAction) => {
   switch (action.type) {
+
+    // action: { type: "ADD_ITEM_TO_INPUT_QUEUE", payload: { input: "Shoot", type: 2 }}
     case actions.ADD_ITEM_TO_INPUT_QUEUE:
-      let newInputQueue = state.inputQueue;
-      newInputQueue.push(action.payload);
-      return Object.assign({}, state, { inputQueue: newInputQueue });
+      console.log('reducers#inputReducers: ADD_ITEM_TO_INPUT_QUEUE: action: ', action);
+      return Object.assign({}, state, {
+        inputQueue: state.inputQueue.concat(action.payload),
+      });
 
     // action: {type: "CLEAR_INPUT_QUEUE"}
     case actions.CLEAR_INPUT_QUEUE:
@@ -66,7 +73,5 @@ const inputReducers = (state: AppStoreState = initialState, action: Action<any>)
 export const reducers = combineReducers({
   ui: uiStateReducers,
   input: inputReducers,
+  debug: debugReducers,
 });
-
-console.log('reducers.tsx:: single reducer:: const uiStateReducers: ', uiStateReducers);
-console.log('reducers.tsx:: combined reducers:: const reducers:: ', reducers);
